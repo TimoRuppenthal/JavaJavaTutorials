6554ee976666df0e0ce831d2e75dc38310f78520
Timo <89629977+TimoRuppenthal@users.noreply.github.com>
2021-09-21 10:29:35 +0200

    .

---
+++ src/Tutorial_11/ForSchleife.java
@@ -0,0 +1,10 @@
+package Tutorial_11;
+
+public class ForSchleife {
+    public static void main(String[] args)
+    {
+       for(int i = 5; i < 10; i++) {
+           System.out.println(i * 2);   //Zählervariable kann auch bspw. mit 2 multipliziert werden
+       }
+    }
+}
