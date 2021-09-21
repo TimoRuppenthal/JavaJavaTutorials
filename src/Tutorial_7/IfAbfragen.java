78cca63018168c23c2149cc0f9fa1531c1428f8a
Timo <89629977+TimoRuppenthal@users.noreply.github.com>
2021-09-21 08:19:39 +0200

    .

---
+++ src/Tutorial_7/IfAbfragen.java
@@ -0,0 +1,17 @@
+package Tutorial_7;
+
+public class IfAbfragen {
+    public static void main(String[] args)
+    {
+      int age = 17;
+      age = age + 1;
+
+      if(age > 17){
+          System.out.println("Dann darfst Du das Computerspiel ab 18 Jahren kaufen!" );
+      }
+      else{
+          System.out.println("Du darfst das Spiel leider nicht kaufen");
+
+      }
+    }
+}
