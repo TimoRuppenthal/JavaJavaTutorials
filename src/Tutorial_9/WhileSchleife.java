7f6dd279d15e690e1e4f132586aabc9942b65db1
Timo <89629977+TimoRuppenthal@users.noreply.github.com>
2021-09-21 09:19:24 +0200

    .

---
+++ src/Tutorial_9/WhileSchleife.java
@@ -0,0 +1,13 @@
+package Tutorial_9;
+
+public class WhileSchleife {
+    public static void main(String[] args)
+    {
+        int number = 1;
+
+        while(number<=5){
+            System.out.println(number);
+            number += 1; //number = number + 1;  kombinierter Zuweisungsoperator,  Kurzschreibweise, um die "1" zu erhöhen geht auch ++
+        }
+    }
+}
