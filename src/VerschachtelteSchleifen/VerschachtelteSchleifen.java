e0989a29949550b400c256e823f398fb460ca80c
Timo <89629977+TimoRuppenthal@users.noreply.github.com>
2022-01-03 10:43:56 +0100

    ForeachSchleife

--- src/VerschachtelteSchleifen/VerschachtelteSchleifen.java
+++ src/VerschachtelteSchleifen/VerschachtelteSchleifen.java
@@ -3,8 +3,12 @@ package VerschachtelteSchleifen;
 public class VerschachtelteSchleifen {
     public static void main(String[] args)
     {
-       gxgxgggggg
+      for(int i = 1; i < 4; i++){ // 3 Schleifendurchläufe da bis zur 4 erhöht wird
 
-
+          for(int j = 1; j < 4; j++){ // Bei der inneren Schleife auch 3 Durchläufe
+              System.out.print("x "); // Die erste Zeile kommt zustande, da die äußere bei einem Durchlauf einmal ausgeführt wird, die innere allerdings direkt 3 mal
+          }
+          System.out.println();
+      }
     }
 }
